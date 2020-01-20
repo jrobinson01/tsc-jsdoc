59d08f76fb306815911443aaa6fba7e2732a731c
John Robinson <john.robinson@banno.com>
2020-01-20 09:32:05 -0500

    complete example 2

--- packages/package-a/lib/lib-fns.js
+++ packages/package-a/lib/lib-fns.js
@@ -1,11 +1,26 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
 function add(a,b) {
   return a + b;
 }
 
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
 function subtract(a, b) {
   return a - b;
 }
 
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
 function multiply(a, b) {
   return a * b;
 }
