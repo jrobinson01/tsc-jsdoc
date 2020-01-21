89de9a932284fcad81ab2321dbbcd406fa870861
John Robinson <john.robinson@banno.com>
2020-01-21 08:44:21 -0500

    comments

--- packages/package-b/index.js
+++ packages/package-b/index.js
@@ -1,11 +1,10 @@
 import {add, subtract} from 'package-a';
 import {multiply} from 'package-a/lib/lib-fns.js';
 
-// tsc can now determine that the parameters and return are numbers
+// tsc has no idea what is going on now
 add(2, 2);
 
-// and will complain that the second parameter is not a number
+// this should be an error!
 subtract(2, 'banana');
 
-// it also works for absolute imports
 multiply(2, 2);
